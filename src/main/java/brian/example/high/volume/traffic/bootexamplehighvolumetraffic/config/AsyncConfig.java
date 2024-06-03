7f172e28a38c957011b87a7138cb7fd539e13466
allthatjava <allthatjava@yahoo.ca>
2024-06-03 16:20:03 -0400

    Adds additional option for @Async test on Jmeter

--- src/main/java/brian/example/high/volume/traffic/bootexamplehighvolumetraffic/config/AsyncConfig.java
+++ src/main/java/brian/example/high/volume/traffic/bootexamplehighvolumetraffic/config/AsyncConfig.java
@@ -1,20 +1,26 @@
 package brian.example.high.volume.traffic.bootexamplehighvolumetraffic.config;
 
+import brian.example.high.volume.traffic.bootexamplehighvolumetraffic.service.AsyncService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @Configuration
 public class AsyncConfig {
+    private static final Logger logger = LoggerFactory.getLogger(AsyncConfig.class);
+
     @Bean(name="taskExecutor")
-    public Executor taskExecutor(){
+    public ThreadPoolTaskExecutor taskExecutor(){
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(100);
         executor.setMaxPoolSize(1000);
-        executor.setQueueCapacity(50);
+        executor.setQueueCapacity(1000);
         executor.setThreadNamePrefix("Async-");
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         executor.initialize();
         return executor;
     }
--- src/main/java/brian/example/high/volume/traffic/bootexamplehighvolumetraffic/service/AsyncService.java
+++ src/main/java/brian/example/high/volume/traffic/bootexamplehighvolumetraffic/service/AsyncService.java
@@ -2,26 +2,31 @@ package brian.example.high.volume.traffic.bootexamplehighvolumetraffic.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 
 @Service
 public class AsyncService {
+
+    @Autowired
+    private ThreadPoolTaskExecutor executor;
 
     private static final Logger logger = LoggerFactory.getLogger(AsyncService.class);
 
     @Async("taskExecutor")
     public CompletableFuture<String> asyncHello(){
-        logger.info("Start asyncMethod");
+        logger.info("Active Count:{},Queue size:{}, Pool Size:{}",executor.getActiveCount(),executor.getQueueSize(),executor.getPoolSize());
         try{
             Thread.sleep(1000);
         }catch( InterruptedException e){
             Thread.currentThread().interrupt();
         }
-        logger.info("End asyncMethod");
 
         return CompletableFuture.completedFuture("Hello from Async service");
     }
